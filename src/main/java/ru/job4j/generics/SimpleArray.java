ec2c2bf1ebb5229bbf1683da2326c4aef7805690
julja83 <59063644+julja83@users.noreply.github.com>
2021-07-09 17:39:09 +0300

    0. Что такое обобщенные типы (generics)

---
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -0,0 +1,60 @@
+package ru.job4j.generics;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleArray<T> implements Iterable<T> {
+
+  private T[] array;
+  private int point = 0;
+
+  public SimpleArray(int count) {
+    array = (T[]) new Object[count];
+  }
+
+  public Boolean add(T model) {
+    Objects.checkIndex(point, array.length - 1);
+    int i = 0;
+    while (array[i] != null && i != array.length - 1) {
+      i++;
+    }
+    array[i] = model;
+    return true;
+  }
+
+  public void set(int index, T model) {
+    Objects.checkIndex(index, array.length);
+    array[index] = model;
+  }
+
+  public void remove(int index) {
+    Objects.checkIndex(index, array.length);
+    System.arraycopy(array, index + 1, array, index, array.length - index - 1);
+    array[array.length - 1] = null;
+  }
+
+  public T get(int index) {
+    Objects.checkIndex(index, array.length);
+    return array[index];
+  }
+  
+  @Override
+  public Iterator<T> iterator() {
+
+    return new Iterator<T>() {
+      @Override
+      public boolean hasNext() {
+        return point < array.length;
+      }
+
+      @Override
+      public T next() {
+        if (!hasNext()) {
+          throw new NoSuchElementException();
+        }
+        return array[point++];
+      }
+    };
+  }
+}
---
+++ src/test/java/ru/job4j/generics/SimpleArrayTest.java
@@ -0,0 +1,76 @@
+package ru.job4j.generics;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class SimpleArrayTest {
+
+  @Test
+  public void addElement() {
+    SimpleArray<Integer> simpleArray = new SimpleArray<>(5);
+    assertThat(simpleArray.add(1), is(true));
+  }
+
+  @Test(expected = IndexOutOfBoundsException.class)
+  public void addElementWithIndexMoreThenSize() {
+    SimpleArray<String> simpleArray = new SimpleArray<>(1);
+    simpleArray.add("1");
+    simpleArray.add("2");
+  }
+
+  @Test()
+  public void setElement() {
+    SimpleArray<Integer> simpleArray = new SimpleArray<>(2);
+    simpleArray.add(1);
+    simpleArray.add(2);
+    simpleArray.set(0, 3);
+    assertThat(simpleArray.get(0), is(3));
+  }
+
+  @Test(expected = IndexOutOfBoundsException.class)
+  public void setElementWithIndexMoreThenSize() {
+    SimpleArray<Integer> simpleArray = new SimpleArray<>(2);
+    simpleArray.add(1);
+    simpleArray.set(5, 3);
+  }
+
+  @Test()
+  public void getElement() {
+    SimpleArray<Integer> simpleArray = new SimpleArray<>(2);
+    simpleArray.add(1);
+    simpleArray.add(2);
+    assertThat(simpleArray.get(0), is(1));
+  }
+
+  @Test(expected = IndexOutOfBoundsException.class)
+  public void getElementWithIndexMoreThenSize() {
+    SimpleArray<Integer> simpleArray = new SimpleArray<>(2);
+    simpleArray.add(1);
+    simpleArray.add(2);
+    assertThat(simpleArray.get(5), is(1));
+  }
+
+  @Test()
+  public void removeElement() {
+    SimpleArray<Integer> simpleArray = new SimpleArray<>(3);
+    simpleArray.add(1);
+    simpleArray.add(2);
+    simpleArray.add(3);
+    simpleArray.remove(1);
+    assertThat(simpleArray.get(0), is(1));
+    assertThat(simpleArray.get(1), is(3));
+    Assert.assertNull(simpleArray.get(2));
+  }
+
+  @Test(expected = IndexOutOfBoundsException.class)
+  public void removeElementWithIndexMoreThenSize() {
+    SimpleArray<Integer> simpleArray = new SimpleArray<>(2);
+    simpleArray.add(1);
+    simpleArray.remove(2);
+  }
+
+}

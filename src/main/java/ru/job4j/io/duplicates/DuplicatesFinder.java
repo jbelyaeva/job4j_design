ac1637a6a3ebc12913bcb5fabb3e81370a587550
julja83 <julja83@list.ru>
2021-11-01 07:33:54 +0300

    Правки 4.2. Поиск дубликатов

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -7,7 +7,7 @@ import java.nio.file.Path;
 public class DuplicatesFinder {
 
   public static void main(String[] args) throws IOException {
-    DuplicatesVisitor duplicatesVisitor  = new DuplicatesVisitor();
+    DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
     Files.walkFileTree(Path.of("./"), duplicatesVisitor);
     System.out.println(duplicatesVisitor.getResult());
   }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -1,32 +1,46 @@
 package ru.job4j.io.duplicates;
 
-import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-  private Set<FileProperty> box = new HashSet<>();
+  private Map<FileProperty, List<Path>> box = new HashMap<>();
   private List<Path> result = new ArrayList<>();
 
-  public List<Path> getResult() {
-    return result;
-  }
-
   @Override
-  public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+  public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
     FileProperty target = new FileProperty(file.toFile().length(), file.toFile().getName());
-    if (box.contains(target)) {
-      result.add(file);
+    if (box.containsKey(target)) {
+      List<Path> paths = box.get(target);
+      paths.add(file);
+      box.put(target, paths);
     } else {
-      box.add(target);
+      List<Path> newPath = new ArrayList<>();
+      newPath.add(file);
+      box.put(target, newPath);
     }
     return FileVisitResult.CONTINUE;
+
+  }
+
+ public List<Path> getResult() {
+    int i = 0;
+    for (Map.Entry<FileProperty, List<Path>> entry : box.entrySet()) {
+      int countPath = entry.getValue().size();
+      if (countPath > 1) {
+        for (int j = 0; j < countPath; j++) {
+          result.add(entry.getValue().get(j));
+        }
+      }
+      ++i;
+    }
+    return result;
   }
 }

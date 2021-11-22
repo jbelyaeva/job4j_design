e33a11d770456a6a3d74f77636a838943e743807
julja83 <julja83@list.ru>
2021-11-22 07:47:49 +0300

    7. Scanner

---
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -0,0 +1,155 @@
+package ru.job4j.io;
+
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class CSVReader {
+
+  public static void handle(ArgsName argsName) {
+    validations(argsName, 4);
+    List<String> scannerResult = getStringsFromFile(argsName);
+    List<String> tableIndex = getTableFields(scannerResult);
+    List<String> result = prepareScannerResultForWork(scannerResult);
+    List<Integer> indexes = parseFilter(argsName, tableIndex);
+    List<List<String>> mas = getResultAfterFilter(tableIndex, result, indexes);
+    StringBuilder builder = getStringBuilder(mas);
+
+    if (argsName.get("out").equals("stdout")) {
+      recordInConsole(builder);
+    } else {
+      recordInFile(argsName.get("out"), builder);
+    }
+  }
+
+  private static StringBuilder getStringBuilder(List<List<String>> mas) {
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < mas.get(0).size(); i++) {
+      for (int j = 0; j < mas.size(); j++) {
+        builder.append(mas.get(j).get(i));
+        builder.append(";");
+      }
+      builder.setLength(builder.length() - 1);
+      builder.append(System.lineSeparator());
+    }
+    return builder;
+  }
+
+  private static List<List<String>> getResultAfterFilter(List<String> tableIndex, List<String> result, List<Integer> indexes) {
+    List<List<String>> temp = new ArrayList<>();
+    for (int i = 0; i < indexes.size(); i++) {
+      int finalI = i;
+      if (indexes.get(finalI) == 0) {
+        List<String> a = IntStream.range(0, result.size())
+            .filter(n -> n % 4 == 0)
+            .mapToObj(result::get)
+            .collect(Collectors.toList());
+        temp.add(a);
+      } else {
+        List<String> a = IntStream.range(0, result.size())
+            .filter(n -> (n + tableIndex.size() - indexes.get(finalI)) % 4 == 0) //1
+            .mapToObj(result::get)
+            .collect(Collectors.toList());
+        temp.add(a);
+      }
+    }
+    return temp;
+  }
+
+  private static List<Integer> parseFilter(ArgsName argsName, List<String> tableIndex) {
+    String[] filter = argsName.get("filter").split(",");
+    List<Integer> indexes = new ArrayList<>();
+    for (int i = 0; i < tableIndex.size(); i++) {
+      for (int j = 0; j < filter.length; j++) {
+        if (tableIndex.get(i).equals(filter[j])) {
+          indexes.add(i);
+          break;
+        }
+      }
+    }
+    return indexes;
+  }
+
+  private static List<String> prepareScannerResultForWork(List<String> scannerResult) {
+    List<String> result = new ArrayList<>();
+    for (int i = 0; i < scannerResult.size(); i++) {
+      String temp = scannerResult.get(i);
+      if (temp.contains("\r\n")) {
+        String[] a = temp.split("\r\n");
+        result.add(a[0]);
+        result.add(a[1]);
+      } else {
+        result.add(temp);
+      }
+    }
+    return result;
+  }
+
+  private static List<String> getTableFields(List<String> scannerResult) {
+    List<String> tempMas = new ArrayList<>();
+    for (int i = 0; i < scannerResult.size(); i++) {
+      String temp = scannerResult.get(i);
+      if (!temp.contains("\r\n")) {
+        tempMas.add(temp);
+      } else {
+        tempMas.add(temp.split("\r\n")[0]);
+        break;
+      }
+    }
+    return tempMas;
+  }
+
+  private static List<String> getStringsFromFile(ArgsName argsName) {
+    List<String> scannerResult = new ArrayList<>();
+    try (Scanner scanner = new Scanner(Paths.get(argsName.get("path")).toFile())
+        .useDelimiter(argsName.get("delimiter"))) {
+      while (scanner.hasNext()) {
+        scannerResult.add(scanner.next());
+      }
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return scannerResult;
+  }
+
+  private static void recordInFile(String target, StringBuilder builder) {
+    try (var writer = new FileWriter(target)) {
+      writer.write(builder.toString());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void recordInConsole(StringBuilder builder) {
+    System.out.println(builder);
+  }
+
+  private static void validations(ArgsName args, int countArgs) {
+    if (args.size() != countArgs) {
+      throw new IllegalArgumentException("Not all args");
+    }
+    File filePath = new File(args.get("path"));
+    if (!filePath.isFile()) {
+      throw new IllegalArgumentException("Directory is wrong");
+    }
+    File fileOut = new File(args.get("out"));
+    if (!fileOut.getName().equals("stdout")) {
+      if (!fileOut.isFile()) {
+        throw new IllegalArgumentException("Directory out is wrong");
+      }
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    ArgsName argsName = ArgsName.of(new String[]{args[0], args[1], args[2], args[3]});
+    CSVReader.handle(argsName);
+  }
+
+}
---
+++ src/test/java/ru/job4j/io/CSVReaderTest.java
@@ -0,0 +1,138 @@
+package ru.job4j.io;
+
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.nio.file.Files;
+
+public class CSVReaderTest {
+
+  @Rule
+  public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+  @Test
+  public void whenFilterTwoColumns() throws Exception {
+    String data = String.join(
+        System.lineSeparator(),
+        "name;age;last_name;education",
+        "Tom;20;Smith;Bachelor",
+        "Jack;25;Johnson;Undergraduate",
+        "William;30;Brown;Secondary special"
+    );
+    File file = temporaryFolder.newFile("source.csv");
+    File target = temporaryFolder.newFile("target.csv");
+    ArgsName argsName = ArgsName.of(new String[]{
+        "-path=" + file.getAbsolutePath(), "-delimiter=;", "-out=" + target.getAbsolutePath(), "-filter=name,age"
+    });
+    Files.writeString(file.toPath(), data);
+    String expected = String.join(
+        System.lineSeparator(),
+        "name;age",
+        "Tom;20",
+        "Jack;25",
+        "William;30"
+    ).concat(System.lineSeparator());
+    CSVReader.handle(argsName);
+    Assert.assertEquals(expected, Files.readString(target.toPath()));
+  }
+
+  @Test
+  public void whenFilterTreeColumns() throws Exception {
+    String data = String.join(
+        System.lineSeparator(),
+        "name;age;last_name;education",
+        "Tom;20;Smith;Bachelor",
+        "Jack;25;Johnson;Undergraduate",
+        "William;30;Brown;Secondary special"
+    );
+    File file = temporaryFolder.newFile("source1.csv");
+    File target = temporaryFolder.newFile("target1.csv");
+    ArgsName argsName = ArgsName.of(new String[]{
+        "-path=" + file.getAbsolutePath(), "-delimiter=;", "-out=" + target.getAbsolutePath(), "-filter=name,age,education"
+    });
+    Files.writeString(file.toPath(), data);
+    String expected = String.join(
+        System.lineSeparator(),
+        "name;age;education",
+        "Tom;20;Bachelor",
+        "Jack;25;Undergraduate",
+        "William;30;Secondary special"
+    ).concat(System.lineSeparator());
+    CSVReader.handle(argsName);
+    Assert.assertEquals(expected, Files.readString(target.toPath()));
+  }
+
+  @Test
+  public void whenFilterOneColumns() throws Exception {
+    String data = String.join(
+        System.lineSeparator(),
+        "name;age;last_name;education",
+        "Tom;20;Smith;Bachelor",
+        "Jack;25;Johnson;Undergraduate",
+        "William;30;Brown;Secondary special"
+    );
+    File file = temporaryFolder.newFile("source.csv");
+    File target = temporaryFolder.newFile("target3.csv");
+    ArgsName argsName = ArgsName.of(new String[]{
+        "-path=" + file.getAbsolutePath(), "-delimiter=;", "-out=" + target.getAbsolutePath(), "-filter=education"
+    });
+    Files.writeString(file.toPath(), data);
+    String expected = String.join(
+        System.lineSeparator(),
+        "education",
+        "Bachelor",
+        "Undergraduate",
+        "Secondary special"
+    ).concat(System.lineSeparator());
+    CSVReader.handle(argsName);
+    Assert.assertEquals(expected, Files.readString(target.toPath()));
+  }
+
+  @Test
+  public void whenFilterChangeColumns() throws Exception {
+    String data = String.join(
+        System.lineSeparator(),
+        "name;age;last_name;education",
+        "Tom;20;Smith;Bachelor",
+        "Jack;25;Johnson;Undergraduate",
+        "William;30;Brown;Secondary special"
+    );
+    File file = temporaryFolder.newFile("source.csv");
+    File target = temporaryFolder.newFile("target4.csv");
+    ArgsName argsName = ArgsName.of(new String[]{
+        "-path=" + file.getAbsolutePath(), "-delimiter=;", "-out=" + target.getAbsolutePath(), "-filter=education,name"
+    });
+    Files.writeString(file.toPath(), data);
+    String expected = String.join(
+        System.lineSeparator(),
+        "name;education",
+        "Tom;Bachelor",
+        "Jack;Undergraduate",
+        "William;Secondary special"
+    ).concat(System.lineSeparator());
+    CSVReader.handle(argsName);
+    Assert.assertEquals(expected, Files.readString(target.toPath()));
+  }
+
+  @Test
+  public void whenFilterTwoColumnsInConsole() throws Exception {
+    String data = String.join(
+        System.lineSeparator(),
+        "name;age;last_name;education",
+        "Tom;20;Smith;Bachelor",
+        "Jack;25;Johnson;Undergraduate",
+        "William;30;Brown;Secondary special"
+    );
+    File file = temporaryFolder.newFile("source.csv");
+    ArgsName argsName = ArgsName.of(new String[]{
+        "-path=" + file.getAbsolutePath(), "-delimiter=;", "-out=stdout", "-filter=name,age"
+    });
+    Files.writeString(file.toPath(), data);
+    CSVReader.handle(argsName);
+  }
+
+}

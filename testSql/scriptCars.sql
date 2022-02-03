9bb7472ef0e2cb8164f74e9df5e2eaa56ce952c6
julja83 <julja83@list.ru>
2022-02-03 07:00:58 +0300

    Хранилище машин

---
+++ testSql/scriptCars.sql
@@ -0,0 +1,107 @@
+
+create table body (
+  id serial primary key,
+	name varchar(255)
+);
+
+create table motor (
+  id serial primary key,
+	name varchar(255)
+);
+
+create table transmission (
+  id serial primary key,
+	name varchar(255)
+);
+
+create table car (
+  id serial primary key,
+	name varchar(255),
+  body_id int references body(id),
+  motor_id int references motor(id),
+  transmission_id int references transmission(id)
+);
+
+insert into body (name) values
+('сидан'),
+('хечбек'),
+('универсал'),
+('кабриолет'),
+('джип');
+
+insert into motor (name) values
+('бензиновый'),
+('дизельный'),
+('электрический');
+
+insert into transmission (name) values
+('автоматическая'),
+('механическая'),
+('роботизированная');
+
+insert into  car (name, body_id, motor_id, transmission_id ) values
+('Машина1', 1, 1, 1),
+('Машина2', 2, 1, 1),
+('Машина3', 3, 1, 1),
+('Машина4', 4, 1, 1),
+('Машина5', 1, 2, 1),
+('Машина6', 1, 3, 1),
+('Машина7', 4, 1, 2),
+('Машина8', 1, 2, 2),
+('Машина9', 1, 1, 1);
+
+
+ select
+ tableBody.id,
+ tableBody.car,
+ tableBody.body,
+ tableMotor.motor,
+ tableTrans.transmission
+ from
+ (select
+ c.id,
+ c.name as car,
+ b.name as body
+ from car c
+ left join body b
+ on c.body_id = b.id) tableBody,
+ (
+ select
+ c.id,
+ c.name as car,
+ m.name as motor
+ from car c
+ left join motor m
+ on c.motor_id = m.id) tableMotor,
+ (
+ select
+ c.id,
+ c.name as car,
+ t.name as transmission
+ from car c
+ left join transmission t
+ on c.transmission_id = t.id) tableTrans
+ where
+ tableBody.id = tableMotor.id
+ and tableBody.id = tableTrans.id;
+
+ select
+ b.name as body
+ from car c
+ right join body b
+ on c.body_id = b.id
+ where c.name is null;
+
+ select
+ m.name as motor
+ from car c
+ right join motor m
+ on c.motor_id = m.id
+ where c.name is null;
+
+ select
+ t.name as transmission
+ from car c
+ right join transmission t
+ on c.transmission_id = t.id
+ where c.name is null;

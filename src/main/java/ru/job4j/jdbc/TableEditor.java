674b0de484a20b10fd2947f00c88e4ff6b7bd9d7
julja83 <julja83@list.ru>
2022-02-17 08:18:19 +0300

    Правки: 0.1. Statement

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -1,7 +1,8 @@
 package ru.job4j.jdbc;
 
-import static ru.job4j.jdbc.Connection.getConnection;
-
+import java.io.FileInputStream;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 import java.util.StringJoiner;
@@ -15,91 +16,74 @@ public class TableEditor implements AutoCloseable {
 
   public TableEditor(Properties properties) throws Exception {
     this.properties = properties;
-    initConnection();
+    initConnection(properties);
   }
 
-  private void initConnection() throws Exception {
-    connection = getConnection();
+  private void initConnection(Properties properties) throws Exception {
+    Class.forName(properties.getProperty("driver"));
+    String url = properties.getProperty("url");
+    String login = properties.getProperty("login");
+    String password = properties.getProperty("password");
+    connection = DriverManager.getConnection(url, login, password);
   }
 
-  public static void createTable(String tableName) {
-    try (java.sql.Connection connection = getConnection()) {
-      try (Statement statement = connection.createStatement()) {
-        String sql = String.format(
-            "create table if not exists %s(%s, %s);",
-            tableName,
-            "id serial primary key",
-            "name text"
-        );
-        statement.execute(sql);
-      }
-    } catch (Exception throwables) {
+  public void createTable(String tableName) {
+    String sql = String.format(
+        "create table if not exists %s(%s, %s);",
+        tableName,
+        "id serial primary key",
+        "name text"
+    );
+    perform(sql);
+  }
+
+  public void dropTable(String tableName) {
+    String sql = String.format(
+        "drop table if exists %s;",
+        tableName
+    );
+    perform(sql);
+  }
+
+  public void addColumn(String tableName, String columnName, String type) {
+    String sql = String.format(
+        "alter table %s add column %s %s;",
+        tableName,
+        columnName,
+        type
+    );
+    perform(sql);
+  }
+
+  public void dropColumn(String tableName, String columnName) {
+    String sql = String.format(
+        "alter table %s drop column %s;",
+        tableName,
+        columnName
+    );
+    perform(sql);
+  }
+
+  public void renameColumn(String tableName, String columnName, String newColumnName) {
+    String sql = String.format(
+        "alter table %s rename column %s to %s;",
+        tableName,
+        columnName,
+        newColumnName
+    );
+    perform(sql);
+  }
+
+  private void perform(String request) {
+    try (Statement statement = connection.createStatement()) {
+      statement.execute(request);
+    } catch (SQLException throwables) {
       throwables.printStackTrace();
     }
   }
 
-  public static void dropTable(String tableName) {
-    try (java.sql.Connection connection = getConnection()) {
-      try (Statement statement = connection.createStatement()) {
-        String sql = String.format(
-            "drop table if exists %s;",
-            tableName
-        );
-        statement.execute(sql);
-      }
-    } catch (Exception throwables) {
-      throwables.printStackTrace();
-    }
-  }
-
-  public static void addColumn(String tableName, String columnName, String type) {
-    try (java.sql.Connection connection = getConnection()) {
-      try (Statement statement = connection.createStatement()) {
-        String sql = String.format(
-            "alter table %s add column %s %s;",
-            tableName,
-            columnName,
-            type
-        );
-        statement.execute(sql);
-      }
-    } catch (Exception throwables) {
-      throwables.printStackTrace();
-    }
-  }
-
-  public static void dropColumn(String tableName, String columnName) {
-    try (java.sql.Connection connection = getConnection()) {
-      try (Statement statement = connection.createStatement()) {
-        String sql = String.format(
-            "alter table %s drop column %s;",
-            tableName,
-            columnName
-        );
-        statement.execute(sql);
-      }
-    } catch (Exception throwables) {
-      throwables.printStackTrace();
-    }
-  }
-
-  public static void renameColumn(String tableName, String columnName, String newColumnName) {
-    try (java.sql.Connection connection = getConnection()) {
-      try (Statement statement = connection.createStatement()) {
-        String sql = String.format(
-            "alter table %s rename column %s to %s;",
-            tableName,
-            columnName,
-            newColumnName
-        );
-        statement.execute(sql);
-      }
-    } catch (Exception throwables) {
-      throwables.printStackTrace();
-    }
-  }
-
-  public static String getTableScheme(java.sql.Connection connection, String tableName) throws Exception {
+  public static String getTableScheme(java.sql.Connection connection, String tableName)
+      throws Exception {
     var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
     var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
     var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
@@ -108,13 +92,13 @@ public class TableEditor implements AutoCloseable {
       try (var selection = statement.executeQuery(String.format(
           "select * from %s limit 1", tableName
       ))) {
-      var metaData = selection.getMetaData();
-      for (int i = 1; i <= metaData.getColumnCount(); i++) {
-        buffer.add(String.format("%-15s|%-15s%n",
-            metaData.getColumnName(i), metaData.getColumnTypeName(i))
-        );
-      }
-    } catch (PSQLException e) {
+        var metaData = selection.getMetaData();
+        for (int i = 1; i <= metaData.getColumnCount(); i++) {
+          buffer.add(String.format("%-15s|%-15s%n",
+              metaData.getColumnName(i), metaData.getColumnTypeName(i))
+          );
+        }
+      } catch (PSQLException e) {
         System.out.println("Таблица отсутствует в бд");
       }
     }
@@ -129,21 +113,25 @@ public class TableEditor implements AutoCloseable {
   }
 
   public static void main(String[] args) throws Exception {
-    try (java.sql.Connection connection = getConnection()) {
-      createTable("device");
-      System.out.println(getTableScheme(connection, "device"));
+    Properties properties = new Properties();
+    FileInputStream fileInputStream;
+    fileInputStream = new FileInputStream("./src/main/resources/app.properties");
+    properties.load(fileInputStream);
+    try (TableEditor tableEditor = new TableEditor(properties)) {
+      tableEditor.createTable("device");
+      System.out.println(getTableScheme(tableEditor.connection, "device"));
 
-      addColumn("device", "description", "text");
-      System.out.println(getTableScheme(connection, "device"));
+      tableEditor.addColumn("device", "description", "text");
+      System.out.println(getTableScheme(tableEditor.connection, "device"));
 
-      renameColumn("device", "description", "desc1");
-      System.out.println(getTableScheme(connection, "device"));
+      tableEditor.renameColumn("device", "description", "desc1");
+      System.out.println(getTableScheme(tableEditor.connection, "device"));
 
-      dropColumn("device", "desc1");
-      System.out.println(getTableScheme(connection, "device"));
+      tableEditor.dropColumn("device", "desc1");
+      System.out.println(getTableScheme(tableEditor.connection, "device"));
 
-      dropTable("device");
-      System.out.println(getTableScheme(connection, "device"));
+      tableEditor.dropTable("device");
+      System.out.println(getTableScheme(tableEditor.connection, "device"));
     }
   }
 }
